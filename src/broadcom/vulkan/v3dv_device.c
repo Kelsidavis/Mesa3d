b9e0b2f27eab2e53ce498034fa57485797b8e279
Kelsi <kelsihates2fa@gmail.com>
2025-12-30 19:36:16 -0800

    v3dv: Add VK_KHR_maintenance10 extension support
    Enable VK_KHR_maintenance10 which provides:
    - rgba4OpaqueBlackSwizzled property (true)
    - resolveSrgbFormatAppliesTransferFunction property (true)
    - resolveSrgbFormatSupportsTransferFunctionControl property (false
      since V3D doesn't support controlling the transfer function)
    - New flags for rendering attachment and resolve operations

--- src/broadcom/vulkan/v3dv_device.c
+++ src/broadcom/vulkan/v3dv_device.c
@@ -207,6 +207,7 @@ get_device_extensions(const struct v3dv_physical_device *device,
       .KHR_maintenance7                     = true,
       .KHR_maintenance8                     = true,
       .KHR_maintenance9                     = true,
+      .KHR_maintenance10                    = true,
       .KHR_map_memory2                      = true,
       .KHR_push_descriptor                  = true,
       .KHR_global_priority                  = true,
@@ -609,6 +610,9 @@ get_features(const struct v3dv_physical_device *physical_device,
 
       /* VK_KHR_maintenance9 */
       .maintenance9 = true,
+
+      /* VK_KHR_maintenance10 */
+      .maintenance10 = true,
 
       /* VK_KHR_push_descriptor */
       .pushDescriptor = true,
@@ -1461,6 +1465,11 @@ get_device_properties(const struct v3dv_physical_device *device,
       /* VK_KHR_maintenance9 */
       .image2DViewOf3DSparse = false,
       .defaultVertexAttributeValue = VK_DEFAULT_VERTEX_ATTRIBUTE_VALUE_ZERO_ZERO_ZERO_ONE_KHR,
+
+      /* VK_KHR_maintenance10 */
+      .rgba4OpaqueBlackSwizzled = true,
+      .resolveSrgbFormatAppliesTransferFunction = true,
+      .resolveSrgbFormatSupportsTransferFunctionControl = false,
 
       /* VK_KHR_push_descriptor */
       .maxPushDescriptors = 32,
